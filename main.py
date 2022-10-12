ee7a82fe60bb1094f951c8582b8d0da0f7e29f1f
jmackSosa <jmackey@potomacschool.org>
2022-10-12 09:34:38 -0400

    time

--- main.py
+++ main.py
@@ -2,21 +2,17 @@
 class player:
 
 #player constructor
-    def __init__(self):
+    def __init__(self , p):
         self.bScore = 0
-        self.sGB = [4] * 7
-        self.sGB[6] = 0
+        self.p1 = p
         self.turn = False
 
     def getScore(self):
         return self.bScore
-
-    def turn(self):
+    def getTurn(self):
         return self.turn
-
     def sTurn(self):
      (self.turn) != (self.turn)
-
 
 
 
@@ -28,23 +24,18 @@ class human:
 
 
 class gameboard:
-        global mGB = [4] * 14
-        global mGB[6] = 0
-        global mGB[13] = 0
-
-
 
 
 
 
 class game:
 
+
     def rBeans(n):
         hBeans = 0
         nPit = n
         hBeans = mGB[n]
         mGB[n] = 0
-
         for x in range(hBeans):
             nPit += 1
             mGB[nPit] += 1
