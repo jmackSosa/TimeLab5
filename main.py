ee4005e468bd9d5bd64ce7716acb5f729cf25ac3
djcmccall <dcayenne-mccall@potomacschool.org>
2022-10-12 08:51:53 -0400

    commit number 4???

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
 
+
+
 class player:
 
-#player constructor
-    def __init__(self ):
-        self.bScore = 0
-        turn = false
-    def getScore(self):
-        return self.bScore
 
-    def cPit(self, num):
+
+    def __init__(self, ):
+        self.bScore = 0
+
 
 
 
 class human:
-    __init__(self, n):
-        name = n
-
 
 class computer:
 
 
-class gameboard:
-    __init__(self):
-        self.mGB = [4] * 14
-        self.mGB[6] = 0
-        self.mGB[13] = 0
-    def rBeans(self, n):
-        handBeans = 0
-        nPit = n
-        handBeans = mGB[n]
-        mGB[n] = 0
-
-        for x in range (handBeans):
-            nPit += 1
-            mGB[nPit] += 1
 
 
 
-
-
-
-class game:
