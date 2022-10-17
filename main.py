5813b036c794b9220f1b5449a8f9a853dfb417cd
jmackSosa <jmackey@potomacschool.org>
2022-10-17 16:59:00 -0400

    time

--- main.py
+++ main.py
@@ -16,39 +16,43 @@ class gameboard:
     def __init__(self):
         self.r, self.c = (2, 7)
         self.GB = [[4] * self.c] * self.r
-        self.GB[0, 0] = 0
+        self.GB[0, 6] = 0
         self.GB[1, 6] = 0
         self.col = 2
         self.row = 6
-    def Move(self, pit):
-        self.mvmnt = pit
-        if (0 < pit < 7):
-            row1 = True
-            self.hBeans = self.GB[0, pit]
-            for x in range(self.hBeans):
-               if(self.mvmnt > 7)
-
-                self.GB[0, pit]
-
-        else:
-            self.hBeans = self.GB[1, pit]
-            row1 = False
-        #print(" player", , "move at pit " + str(pit))
+    def Move(self, r, p):
+        self.row = r
+        self.oRow = r
+        self.pit = p + 1
+        self.hBeans = self.GB[r, p]
 
 
+        while self.hbeans > 0:
+
+            if (self.pit > 6):
+                self.pit = 0
+                if (self.row == 1):
+                    self.row = 0
+                else:
+                    self.row = 1
+            if (0 <= self.pit < 6):
+                self.GB[self.row, self.pit] += 1
+            else:
+                if (self.row == self.oRow):
+                    self.GB[self.row, self.pit] += 1
+
+            self.hbeans -= 1
 
 
-    def getGB(self)
+
+
+    def getGB(self):
         print(self.GB)
 
 
 
 
-
-
-
 class game:
-    self.p1
     def __init__(self, p1, p2, board1):
         self.player1 = p1
         self.player2 = p2
@@ -83,8 +87,9 @@ class game:
 p1 = player(True)
 p2 = player(True)
 
-boar1 = gameboard(10)
+boar1 = gameboard()
 
 game1 = game(p1, p2, boar1)
-
 game1.startGame(p1, p2, boar1)
+
+print(boar1)
